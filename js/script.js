ac702c662a26b2cbbea384add761e5d435f3cae0
vorotnikov <vorotnikov@galaxy-labs.ru>
2017-02-10 17:39:30 +0300

    add function parallax

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 var items = document.querySelectorAll('.parallax');
 
 for (var i = 0; i < items.length; i++ ) {
-
 	var item = items[i];
-	item.classList.add('out');
 
-	var parallax = new Parallax(item);
-	console.log(parallax, item);
+	parallax(item);
 }
 
-function Parallax(elem) {
+function parallax(item) {
+	var current         = item,
+		currentFriction = current.getAttribute('data-friction'),
+		wrap            = current.getBoundingClientRect(),
+		top             = wrap.top + pageYOffset,
+		bottom          = top + wrap.height;
 
-	if( !(this instanceof Parallax) ) {
-		return new Parallax(elem);	
-	}
+	document.addEventListener('scroll', function(e) {
+		var winH      = window.innerHeight,
+			winBottom = pageYOffset + winH,
+			offset    = winBottom - top,
+			diff      = offset*currentFriction;
 
-	this.elem = elem;
-	this.friction = elem.getAttribute('data-friction');
+		window.requestAnimationFrame(function () {
+			
+			if(top < winBottom && bottom >= pageYOffset) {
+				parallaxRun(item, diff);
+			}
+
+			if(bottom < pageYOffset || top > winBottom) {
+				parallaxStop(item);	
+			}
+		});
+	});
 }
 
-Parallax.prototype.run = function() {
-	console.log('run');
+function parallaxRun(item, diff) {
+	item.style.transform = "translate3d(0, " + diff + "%, 0)";
 }
 
-Parallax.prototype.clear = function() {
-	console.log('clear');
+function parallaxStop(item) {
+	item.style.transform = "";	
 }
